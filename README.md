6abe591e91de41139c831331ed62ad125455ab48
Your Name <you@example.com>
2016-02-05 12:10:24 +1300

    add the priority functionality -- works

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 - is it a profound or cosmetic change? big or visual? 
 - where could the change be implemented?
 - is there something like this already? 
+
+
+### othere ideas TODO:
+- let the user set the priority status when creating the todo Item 
+- let users filter or sort by priority status
+- expand the priority feature to have more than 2 levels
+- add a recycle bin for recovering deleted todos
+- add colors, so people can associate different todos with each other
+- add labels for sorting and filtering
+
--- js/views/todo-view.js
+++ js/views/todo-view.js
@@ -20,6 +20,7 @@ var app = app || {};
 			'click .toggle': 'toggleCompleted',
 			'dblclick label': 'edit',
 			'click .edit-btn': 'edit',
+			'click .priority-btn': 'togglePriority',
 			'click .destroy': 'clear',
 			'keypress .edit': 'updateOnEnter',
 			'keydown .edit': 'revertOnEscape',
@@ -51,9 +52,10 @@ var app = app || {};
 
 			this.$el.html(this.template(this.model.toJSON()));
 			this.$el.toggleClass('completed', this.model.get('completed'));
+			this.$el.toggleClass('priority', this.model.get('priority'));
 			this.toggleVisible();
 			this.$input = this.$('.edit');
-			console.log("t",this)
+			//console.log("t",this)
 			return this;
 		},
 
@@ -65,8 +67,13 @@ var app = app || {};
 			return this.model.get('completed') ?
 				app.TodoFilter === 'active' :
 				app.TodoFilter === 'completed';
-		},
+		}, 
 
+		// Toggle the `"priority"` state of the model.
+		togglePriority: function () {
+			this.model.togglePriority();
+		},
+		
 		// Toggle the `"completed"` state of the model.
 		toggleCompleted: function () {
 			this.model.toggle();
@@ -77,7 +84,7 @@ var app = app || {};
 			this.$el.addClass('editing');
 			this.$input.focus();
 		},
-
+		
 		// Close the `"editing"` mode, saving changes to the todo.
 		close: function () {
 			var value = this.$input.val();
